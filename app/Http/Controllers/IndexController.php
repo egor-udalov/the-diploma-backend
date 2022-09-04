0402c8381e841107460049427475a81957275b06
egor-udalov <zoraudalov@gmail.com>
2022-09-04 20:48:20 +0300

    шаблоны страниц home, login  добавленна вёрстка страниц home, login и контроллер к ним

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	namespace App\Http\Controllers;
+	use App\Http\Controllers\Controller;
+    use App\Models\Page;
+
+    class IndexController extends Controller{
+
+        public function homeAction(){
+
+            return view('pages.home');
+
+        }
+
+        public function loginAction(){
+
+            return view('pages.login');
+
+        }
+
+    }

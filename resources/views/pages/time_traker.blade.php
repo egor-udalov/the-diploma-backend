6c941601c67ec20e6b2ba09402877f2dbebe9aa6
egor-udalov <zoraudalov@gmail.com>
2022-09-18 16:14:43 +0300

    Добавлен функционал таймера

--- resources/views/pages/time_traker.blade.php
+++ resources/views/pages/time_traker.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Add project</title>
+    <meta name="description" content="Add project">
+    <meta name="theme-color" content="#ffffff">
+    
+        <link rel="stylesheet" id="js-startup-stylesheet" href="assets/css/water/light.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+        crossorigin="anonymous" 
+        referrerpolicy="no-referrer" />
+    <link rel="manifest" href="assets/manifest.json">
 
-    <script type="module" nonce="ec7cadfe0e088cb31da4efbffff5539f">
-    	import {GlobalEvents} from '/js/global_events.js?version=1.3.3';
+    <script type="module" nonce="38a23a60d181d19aff39970c9765a67f">
+    	import {GlobalEvents} from 'assets/js/global_events.js';
     </script>
 </head>
 <body>
     <div class="page">
+
+<div class="app-menu">
+        <a href="/overview" data-path="/overview">Home</a>
+        <a href="/project" data-path="/project">Projects</a>
+        <a href="/settings" data-path="/settings">Settings</a>
+        <a href="/account/signout" data-path="/account/signout">Sign out</a>
+        <a id="timer_toggle" title="Toggle timer" href="#">&#128337; Timer</a>
+</div>
+
+<div id="timer" class="timer" >
+    <span id="timer_display"></span>
+    <button id="timer_start" class="button-timer button-small" href="#">Start</button>
+    <button id="timer_pause" class="button-timer button-small" href="#">Pause</button>
+    <button id="timer_reset" class="button-timer button-small" href="#">Reset</button>
+</div>
+
+<script src="assets/js/timer.js" nonce="38a23a60d181d19aff39970c9765a67f" ></script>
+<div class="flash-messages">
 
 </div><h3 class="sub-menu">Add project</h3>
 
@@ -26,28 +55,10 @@
     <div class="loadingspinner hidden"></div>
 </form>
 
-<div class="project_list">
-    <ul>
-        <li></li>
-    </ul>
-</div>
-
-<div id="timer" class="timer" >
-    <span id="timer_display"></span>
-    <button id="timer_start" class="button-timer button-small" href="#">Start</button>
-    <button id="timer_pause" class="button-timer button-small" href="#">Pause</button>
-    <button id="timer_reset" class="button-timer button-small" href="#">Reset</button>
-</div>
-
-
-
-<script src="/js/timer.js?v=?version=1.3.3" nonce="ec7cadfe0e088cb31da4efbffff5539f" ></script>
-<div class="flash-messages">
-
-<script type="module" nonce="ec7cadfe0e088cb31da4efbffff5539f">
+<script type="module" nonce="38a23a60d181d19aff39970c9765a67f">
     import {
         Pebble
-    } from '/js/pebble.js?v=1.3.3';
+    } from 'assets/js/pebble.js';
 
     const title = document.getElementById('title');
     title.focus();
@@ -86,7 +97,7 @@
 </div>
 
 <div class="footer">
-       
+    <hr />        
 </div>
 
 </body>

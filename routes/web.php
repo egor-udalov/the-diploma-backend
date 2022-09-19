6df0913266911fff8d80721416698ba603f68ed3
PubliusNovus <Alexey180NN@mail.ru>
2022-09-19 17:18:53 +0300

    Вход на страницу тайм трекера теперь требует пароль

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,18 @@ Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/tables/users', 'tablesUsersAction')->name('tablesUsers');
     Route::get('/admin/tables/usertasks', 'tablesTasksAction')->name('tablesTasks');
 });
-//->middleware(['auth'])->name('pages.home'); //временно закомментиовано
+
+Route::get('/', [IndexController::class, 'homeAction']);
+Route::controller(IndexController::class)->middleware(['auth'])->group(function () {
+    // Route::get('/', 'homeAction');
+    Route::get('/login', 'loginAction');
+    Route::get('/time_traker', 'timeTrakerAction');
+    Route::get('/add_time', 'addTimeAction');
+});
 
 // Route::get('/', function () {
 //     return view('pages.admin.home');
 // })->middleware(['auth'])->name('pages.home'); 
 
-
 require __DIR__.'/auth.php'; 
-
-Route::controller(IndexController::class)->group(function () {
-
-    Route::get('/', 'homeAction');
-    Route::get('/login', 'loginAction');
-    Route::get('/time_traker', 'timeTrakerAction');
-    Route::get('/add_time', 'addTimeAction');
-});
 
